809c3faaf07810f9349272c2af32c22f2af5a148
Rtg000 <rtergom310@g.educaand.es>
2023-11-09 10:16:54 +0100

    importacion y almacenamiento de archivos

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import os
 from datetime import datetime
 from flask import Flask, render_template, request
 import functions as f
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads/'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "super secret key"
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 # Replace the existing home function with the one below
 @app.route("/")
@@ -25,6 +35,22 @@ def csimetrico():
             return render_template('csimetrico.html', decrypted_message=decrypted_message, mode=mode)
 
     return render_template("csimetrico.html")
+
+@app.route("/testingAsim", methods=['GET', 'POST'])
+def testingAsim():
+    if request.method == 'POST':
+        # Revisa si no se ha seleccionado ningún archivo
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # Guarda el archivo en caso de que sea válido
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # return redirect(url_for('download_file', name=filename))
+
+    return render_template("testingAsim.html")
 
 @app.route("/casimetrico/")
 def casimetrico():
