09f99f1922c817719bf00c946fb7d1c267232773
marco279 <markssarango209@gmail.com>
2023-11-22 18:24:46 +0100

    trabajo en equipo

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ from contextlib import _RedirectStream, redirect_stderr
 from curses import flash
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, send_file, url_for
 import functions as f
+import platform
+# if platform.system() != 'Windows':
+from curses import flash
 from werkzeug.utils import secure_filename
 
 
 UPLOAD_FOLDER = './uploads/'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'txt','gpg', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = "super secret key"
+
 def allowed_file(filename):
     # Verifica y crea el directorio de carga si no existe
-    # if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    # os.makedirs(app.config['UPLOAD_FOLDER'])
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
     
@@ -42,6 +46,17 @@ def csimetrico():
 
     return render_template("csimetrico.html")
 
+# registro para guardarlo en un fichero con la ruta a una carpeta de descarga y el nombre del fichero.
+@app.route("/UPLOAD/<filename>")
+def download_file(filename):
+    file_path = f'UPLOAD/{filename}'
+    with app.app_context():
+        print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment= True );
+
+# <a href="[{url_for('download_file', filename=filename) }]">Descargar</a>
+
+
 # @app.route("/testingAsim", methods=['GET', 'POST'])
 # def testingAsim():
 #     if request.method == 'POST':
@@ -54,15 +69,9 @@ def csimetrico():
 #         if file and allowed_file(file.filename):
 #             filename = secure_filename(file.filename)
 #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-#                 #return _RedirectStream(url_for('download_file', name=filename))
+#             return _RedirectStream(url_for('download_file', name=filename))
 #     return render_template("testingAsim.html")
 
-# registro para guardarlo en un fichero con la ruta a una carpeta en local.
-@app.route("/download/<filename>")
-def download_file(filename):
-    with app.app_context():
-        print(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment= True );
 
 @app.route("/casimetrico/")
 def casimetrico():
--- functions.py
+++ functions.py
@@ -13,3 +13,34 @@ def decrypt_message(message, key):
     cipher = AES.new(pad(key, AES.block_size), AES.MODE_ECB)
     decrypted_message = unpad(cipher.decrypt(b64decode(message)), AES.block_size).decode('utf-8')
     return decrypted_message
+
+# Encriptado del archivo.
+
+def encrypt_file(file_path,key):
+    key = key.encode('utf-8')
+    # Lee el contenirdo del archivo para antes de encriptarlo
+    with open(file_path,'rb') as file:
+        original_data=file.read()
+    #Crea un objeto AES en modo ecb con la clave proporcionada y realiza el relleno del mensaje a encriptar
+    cipher = AES.new(pad(key.AES.block_size), AES.MODE_ECB)
+    # Esta parte encripta el mensaje del archivo y lo guarda en un formato base64.
+    encrypted_data = b64encode(cipher.encrypt(pad(original_data, AES.block_size))).decode('utf-8')
+    # Aqui se guardara el archivo una vez ya encriptado.
+    with open("archivo_encriptado.gpg",'w') as encrypted_file:
+        encrypt_file.write(encrypted_data)
+
+# Desencriptado del archivo.
+
+def decrypt_file(encrypted_file_path,key):
+    key = key.encode('utf-8')
+    # Lee el contenido del arcrhivo encriptado
+    with open(encrypted_file_path,'r') as file:
+        encrypted_data = file.read()
+    #Crea un objeto aes en modo ecb con la clave que proporcionamos y este procede a completar el mensaje.
+    cipher = AES.new(pad(key,AES.block_size),AES.MODE_ECB)
+    #Desencripta el mensaje del archivo y realiza el despad.
+    decrypted_data = unpad(cipher.decrypt(b64decode(encrypted_data)),AES.block_size)
+    #Guarda el archivo desencriptadoen un archivo (.txt)
+    with open("archivo_descencriptado.txt",'wb') as decrypted_file:
+        decrypted_file.write(decrypted_data)
+        
