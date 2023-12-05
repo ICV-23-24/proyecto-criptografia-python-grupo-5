1ca5aa9192f63750715382e474df0d4cb17e8abd
Rtg000 <rtergom310@g.educaand.es>
2023-12-05 11:52:59 +0100

    limpieza y orden general de codigo

--- app.py
+++ app.py
@@ -8,28 +8,30 @@ from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 import base64
 
+# Directorio de almacenamiento de claves
 UPLOAD_FOLDER = './uploads/'
-# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+# Extensiones permitidas para la subida de archivos
 ALLOWED_EXTENSIONS = {'pem'}
 
+# Listado de archivos del directorio de almacenamiento de claves
 list_file = os.listdir(UPLOAD_FOLDER)
 list_publickey = fnmatch.filter(list_file, '*_public.pem')
 list_button = list_publickey
 
-
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# app.secret_key = "super secret key"
 
+# Comprobación de validez de subida de archivo
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# Replace the existing home function with the one below
+# Reemplaza la ruta raiz
 @app.route("/")
 def home():
     return render_template("home.html")
 
+# Cifrado simétrico
 @app.route("/csimetrico/", methods=['GET','POST'])
 def csimetrico():
     if request.method == 'POST':
@@ -46,11 +48,11 @@ def csimetrico():
 
     return render_template("csimetrico.html")
 
-@app.route("/testingAsim", methods=['GET', 'POST'])
-def testingAsim():
+# Cifrado Asimétrico
+@app.route("/casimetrico", methods=['GET', 'POST'])
+def casimetrico():
     if request.method == 'POST':
         mode = request.form['mode']
-        # filedownload = request.form["{{ file }}"]
         if mode == 'upload':
             file = request.files['file']
             # Revisa si no se ha seleccionado ningún archivo
@@ -63,27 +65,17 @@ def testingAsim():
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
                 # return redirect(url_for('download_file', name=filename))
         if mode == 'list':
-            return render_template('testingAsim.html',list_button=list_button,mode=mode)
-        if mode == "download":
-            # file = UPLOAD_FOLDER+request.form[{{ file }}]
-            # return render_template('testingAsim.html',mode=mode)
-            # return send_file(file=file,as_attachment=True)
-            # for file in list_file:
-            #     return send_file(UPLOAD_FOLDER+file)
-
-            return send_file(UPLOAD_FOLDER+"Jaja_private.pem")
+            return render_template('casimetrico.html',list_button=list_button,list_publickey=list_publickey,mode=mode)
         if mode == 'generate':
             pem_name = request.form['pem_name']
-            #Generating private key (RsaKey object) of key length of 1024 bits
+            # Genera clave privada con algoritmo RSA con tamaño de 1024 bits
             private_key = RSA.generate(1024)
-            #Generating the public key (RsaKey object) from the private key
+            # Genera la clave pública(RsaKey) desde la clave privadas
             public_key = private_key.publickey()
-
-            #Converting the RsaKey objects to string 
+            # Convierte los RsaKey a string
             private_pem = private_key.exportKey().decode()
             public_pem = public_key.exportKey().decode()
-
-            #Writing down the private and public keys to 'pem' files
+            # Guarda las claves pública y privada en archivos .pem
             public_pem_name = pem_name+'_public.pem'
             private_pem_name = pem_name+'_private.pem'
             
@@ -91,62 +83,41 @@ def testingAsim():
                 pr.write(private_pem)
             with open(UPLOAD_FOLDER+public_pem_name, 'w') as pu:
                 pu.write(public_pem)
-            return render_template('testingAsim.html',private_key=private_key,public_key=public_key,private_pem=private_pem,public_pem=public_pem,mode=mode)
-        if mode == 'import':            
-            #Importing keys from pem files, converting it into the RsaKey object   
-            with open('uploads/private_pem.pem', 'r') as pr_pem:
-                pr_key_pem = pr_pem.read()
-                # global pr_key
-                pr_key = RSA.importKey(pr_key_pem)
-
-            with open('uploads/public_pem.pem', 'r') as pu_pem:
-                pu_key_pem = pu_pem.read()
-                # global pu_key
-                pu_key = RSA.importKey(pu_key_pem)
-                
-            # return render_template('testingAsim.html',pu_key=pu_key,pr_key=pr_key,mode=mode)
-            return render_template('testingAsim.html',pr_key_pem=pr_key_pem,pu_key=pu_key,pr_key=pr_key,pu_key_pem=pu_key_pem,mode=mode)
+            return render_template('casimetrico.html',private_key=private_key,list_publickey=list_publickey,mode=mode)
         if mode == 'encrypt':
-            # global selection
             selection = request.form['selection']
+            # Obtiene el nombre asociado a los archivos de claves
             global name_only
             name_only = selection.split('_')[0]
             with open(UPLOAD_FOLDER+selection, 'r') as pu_pem:
                 pu_key_pem = pu_pem.read()
                 pu_key = RSA.importKey(pu_key_pem)
-            # pu_key = request.form['pu_key']
-            # pu_key = bytes(pu_key,'utf-8')
             message = request.form['message']
             message = bytes(message,'utf-8')
-            #Instantiating PKCS1_OAEP object with the public key for encryption
+            # Utiliza el protocolo de encriptación PKCS1_OAEP para cifrar con la clave pública
             cipher = PKCS1_OAEP.new(pu_key)
-            #Encrypting the message with the PKCS1_OAEP object
             global cipher_text
+            # Encripta el mensaje
             cipher_text = cipher.encrypt(message)
+            # Codifica el mensaje en B64
             cipher_text_b64 = base64.b64encode(cipher_text).decode('utf-8')
-            return render_template('testingAsim.html',cipher_text=cipher_text,cipher_text_b64=cipher_text_b64,list_file=list_file,name_only=name_only,mode=mode)
-        
+            return render_template('casimetrico.html',cipher_text_b64=cipher_text_b64,name_only=name_only,list_publickey=list_publickey,mode=mode)
         if mode == 'decrypt':
-            # selection = request.form['selection']
-            # selection = UPLOAD_FOLDER+selection
+            # Recibe el nombre asociado a los archivos de claves
             associated_private_key_name = name_only+'_private.pem'
+            # Importa la clave privada
             with open(UPLOAD_FOLDER+associated_private_key_name, 'r') as pr_pem:
                 pr_key_pem = pr_pem.read()
                 pr_key = RSA.importKey(pr_key_pem)
             encrypted_message_b64 = request.form['encrypted_message']
+            # Convierte el mensaje de B64 a bytes
             encrypted_message = base64.b64decode(encrypted_message_b64)
-            #Instantiating PKCS1_OAEP object with the public key for encryption
+            # Utiliza el protocolo de encriptación PKCS1_OAEP para descifrar con la clave privada
             decipher = PKCS1_OAEP.new(pr_key)
-            #Encrypting the message with the PKCS1_OAEP object
+            # Descifra el mensaje
             decipher_text = decipher.decrypt(encrypted_message)
-            # return render_template('testingAsim.html',encrypted_message=encrypted_message,decipher_text=decipher_text,mode=mode)
-            return render_template('testingAsim.html',encrypted_message=encrypted_message,encrypted_message_b64=encrypted_message_b64,decipher_text=decipher_text,mode=mode)
-    return render_template("testingAsim.html",list_publickey=list_publickey)
-
-@app.route("/casimetrico/")
-def casimetrico():
-    return render_template("casimetrico.html")
-
+            return render_template('casimetrico.html',decipher_text=decipher_text,list_publickey=list_publickey,mode=mode)
+    return render_template("casimetrico.html",list_publickey=list_publickey)
 
 @app.route("/about/")
 def about():
@@ -156,27 +127,12 @@ def about():
 def doc():
     return render_template("doc.html")
 
-@app.route("/otro/")
-def otro():
-    return render_template("otro.html")
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
 
+# Ruta de descarga de archivos
 @app.route('/uploads/<path:file>', methods=['GET', 'POST'])
 def download(file):
     uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
-    # return send_from_directory(directory=uploads,file=file, as_attachment=True)
     return send_file(uploads+file)
